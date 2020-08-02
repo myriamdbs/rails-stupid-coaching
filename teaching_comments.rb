512af91bb2db9844cd1cb313d0133b5bad77740f
Myriam Dubois <myriam.dubois@skema.edu>
2020-08-02 16:16:19 +0200

    test added

---
+++ teaching_comments.rb
@@ -0,0 +1,4 @@
+# for testing part  :
+# if bug
+# /Users/myriam1/code/myriamdbs/rails-stupid-coaching/db/schema.rb doesn't exist yet. Run `rails db:migrate` to create it, then try again. If you do not intend to use a database, you should instead alter /Users/myriam1/code/myriamdbs/rails-stupid-coaching/config/application.rb to limit the frameworks that will be loaded.
+# run rails db:migrate
--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -11,4 +11,17 @@ class QuestionsTest < ApplicationSystemTestCase
 
     assert_text "I don't care, get dressed and go to work!"
   end
+
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "getting happy answer fom coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great"
+  end
 end
